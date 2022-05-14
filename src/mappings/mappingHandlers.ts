7831133eb9cc88bd94dd0588ebc3c1e5358b7b6c
lbj <lbj@lbjdeMacBook-Pro.local>
2022-05-14 16:24:39 +0800

    final version

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,22 +1,32 @@
-import { SubstrateEvent } from "@subql/types";
+import { SubstrateEvent, SubstrateExtrinsic } from "@subql/types";
 import { Transfer } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 
-export async function handleTransfer(event: SubstrateEvent): Promise<void> {
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
     // Get data from the event
     // The balances.transfer event has the following payload \[from, to, value\]
-    // logger.info(JSON.stringify(event));
-    const from = event.event.data[0];
-    const to = event.event.data[1];
-    const amount = event.event.data[2];
-    
+    logger.info(JSON.stringify(event));
+    const fromAccount = event.event.data[0];
+    const toAccount = event.event.data[1];
+    const balanceChange = event.event.data[2];
+
     // Create the new transfer entity
     const transfer = new Transfer(
         `${event.block.block.header.number.toNumber()}-${event.idx}`,
     );
     transfer.blockNumber = event.block.block.header.number.toBigInt();
-    transfer.from = from.toString();
-    transfer.to = to.toString();
-    transfer.amount = (amount as Balance).toBigInt();
+    transfer.fromAccount = fromAccount.toString();
+    transfer.toAccount = toAccount.toString();
+    transfer.balanceChange = (balanceChange as Balance).toBigInt();
     await transfer.save();
+}
+
+export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const record = await Transfer.get(extrinsic.block.block.header.hash.toString());
+    if (record) {
+        //Date type timestamp
+        logger.info("==timestamp=" + extrinsic.block.timestamp);
+        record.timestamp = extrinsic.block.timestamp;
+        await record.save();
+    }
 }
